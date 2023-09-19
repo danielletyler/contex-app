fcf66ae0bf760a6765a5b498d18668f6464afe00
danielletyler <danielletyler7@me.com>
2023-09-19 14:57:58 -0500

    trying

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,6 @@ import Config
 # before starting your production server.
 config :contex_app, ContexAppWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "https://warm-fluffy-copepod.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
 
 # Configures Swoosh API Client
@@ -22,3 +21,14 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :contex_app, ContexAppWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: "warm-fluffy-copepod.gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :contex_app, ContexApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
