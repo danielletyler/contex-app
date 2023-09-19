d1a49049316922f5d9af3d2554d56b6f24c89493
danielletyler <danielletyler7@me.com>
2023-09-19 13:45:42 -0500

    check origin

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ import Config
 # before starting your production server.
 config :contex_app, ContexAppWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "https://warm-fluffy-copepod.gigalixirapp.com/", port: 443, scheme: "https"],
+  check_origin: ["https://warm-fluffy-copepod.gigalixirapp.com", "//warm-fluffy-copepod.gigalixirapp.com"]
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
